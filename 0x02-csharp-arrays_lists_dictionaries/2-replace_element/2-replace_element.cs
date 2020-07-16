7b7dab5de042ac840918f5b9a7bc25ff2eebc2f7
Anthony <huggins9000211@gmail.com>
2020-07-16 17:22:07 -0400

    test

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+    class Array
+    {
+        public static int[] ReplaceElement(int[] array, int index, int n)
+        {
+            try
+            {
+                array.SetValue(n, index);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index out of range");
+                return(array);
+            }
+            return (array);
+        }
+    }
