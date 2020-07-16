bf6f627ab9df346d56efe848f8165894663a1282
Anthony <huggins9000211@gmail.com>
2020-07-16 16:51:42 -0400

    test

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] myArray = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            if (i == size - 1)
+            {
+                Console.WriteLine(i);
+                myArray[i] = i;
+            }
+            else
+            {
+                Console.Write("{0} ", i);
+                myArray[i] = i;
+            }
+            
+        }
+        return myArray;
+    }
+}
