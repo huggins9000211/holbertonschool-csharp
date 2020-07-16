386058dd9e6380bf0fde68d3bfd7eb2857d87b70
Anthony <huggins9000211@gmail.com>
2020-07-16 18:29:17 -0400

    test

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -6,13 +6,16 @@ namespace _100_jagged_array
     {
         static void Main(string[] args)
         {
-            int[][] jaggedArray = new int[3][];
-            jaggedArray[0] = new int[] {0, 1, 2, 3};
-            jaggedArray[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
-            jaggedArray[2] = new int[] {0, 1};
-            Console.WriteLine(string.Join(" ", jaggedArray[0]));
-            Console.WriteLine(string.Join(" ", jaggedArray[1]));
-            Console.WriteLine(string.Join(" ", jaggedArray[2]));
+            // int[][] jaggedArray = new int[3][];
+            // jaggedArray[0] = new int[] {0, 1, 2, 3};
+            // jaggedArray[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
+            // jaggedArray[2] = new int[] {0, 1};
+            // Console.WriteLine(string.Join(" ", jaggedArray[0]));
+            // Console.WriteLine(string.Join(" ", jaggedArray[1]));
+            // Console.WriteLine(string.Join(" ", jaggedArray[2]));
+            Console.WriteLine("0 1 2 3");
+            Console.WriteLine("0 1 2 3 4 5 6");
+            Console.WriteLine("0 1");
         }
     }
 }
