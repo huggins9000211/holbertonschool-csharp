72b7aa400dd9a0a42f914858976a476affe21965
Anthony <huggins9000211@gmail.com>
2020-07-16 19:47:33 -0400

    test

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> kList = new List<string>(myDict.Keys);
+        kList.Sort();
+
+        foreach (var key in kList)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        }
+    }
+}
