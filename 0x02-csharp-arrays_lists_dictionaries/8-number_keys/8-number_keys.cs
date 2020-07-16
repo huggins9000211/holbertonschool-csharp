fa606eddd3a2f8fcf924ef095ab8592120029659
Anthony <huggins9000211@gmail.com>
2020-07-16 19:16:45 -0400

    test

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        Dictionary<string, string>.KeyCollection keyColl = myDict.Keys;
+
+        // The elements of the KeyCollection are strongly typed
+        // with the type that was specified for dictionary keys.
+        int i = 0;
+        foreach( string s in keyColl )
+        {
+            i++;
+        }
+        return i;
+    }
+}
