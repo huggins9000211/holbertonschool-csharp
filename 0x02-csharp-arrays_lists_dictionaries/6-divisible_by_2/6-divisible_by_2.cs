d4cee31c546d05f9f16c222604ea562524d6b8aa
Anthony <huggins9000211@gmail.com>
2020-07-16 19:03:00 -0400

    test

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> myL = new List<bool>();
+        foreach (int x in myList)
+        {
+            if (x % 2 == 0)
+            {
+                myL.Add(true);
+            }
+            else
+            {
+                myL.Add(false);
+            }
+        }
+    }
+}
+
