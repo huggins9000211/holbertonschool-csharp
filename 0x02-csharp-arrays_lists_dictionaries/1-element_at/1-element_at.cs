622740312d43b7e348f75194cd1fc8099d3e2e51
Anthony <huggins9000211@gmail.com>
2020-07-16 17:15:55 -0400

    test

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        int x;
+        try
+        {
+            x = (int)array.GetValue(index);
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Index out of range");
+            return(-1);
+        }
+        return (x);
+    }
+}
