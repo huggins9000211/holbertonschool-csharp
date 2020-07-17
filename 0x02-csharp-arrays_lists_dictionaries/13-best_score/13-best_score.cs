40ec2a10b3cf235550eead34140c657a1a77c045
Anthony <huggins9000211@gmail.com>
2020-07-16 20:04:55 -0400

    test

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int i = -1;
+        string answer = "None";
+        foreach(KeyValuePair<string, int> item in myList)
+        {
+            if (item.Value > i)
+            {
+                i = item.Value;
+                answer = item.Key;
+            }
+        }
+        return answer;
+    }
+}
