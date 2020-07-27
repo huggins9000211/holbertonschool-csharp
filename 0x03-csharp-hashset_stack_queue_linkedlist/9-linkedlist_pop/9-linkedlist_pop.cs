1a6051ad15e547f176458fafeb3a75e036bf984c
Anthony <huggins9000211@gmail.com>
2020-07-27 15:48:40 -0400

    test

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+
+        LinkedListNode<int> x = myLList.First;
+        try
+        {
+            myLList.RemoveFirst();
+        }
+        catch (InvalidOperationException)
+        {
+            return 0;
+        }
+        return x.Value;
+    }
+}
