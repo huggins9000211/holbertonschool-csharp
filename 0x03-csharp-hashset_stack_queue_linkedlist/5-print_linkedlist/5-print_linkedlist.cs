bd699c818cd6ff3787a34ce77fa38779c1ca3cf5
Anthony <huggins9000211@gmail.com>
2020-07-27 14:48:05 -0400

    test

--- 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -7,6 +7,10 @@ class LList
     public static LinkedList<int> CreatePrint(int size)
     {
         LinkedList<int> newL = new LinkedList<int>();
+        if (size < 0)
+        {
+            return newL;
+        }
         foreach (int value in Enumerable.Range(0, size))
         {
             Console.WriteLine(value);
