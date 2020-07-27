1a2fd359995b29d38eb68e45bccc3a0de3eb1b18
Anthony <huggins9000211@gmail.com>
2020-07-27 14:29:02 -0400

    test

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        try
+        {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        aQueue.Queue(newItem); 
+        if (aQueue.Contains(search))
+        {
+            Console.WriteLine("Queue contains \"{0}\": True", search);
+            string temp = null;
+            while (temp != search)
+            {
+                temp = aQueue.Dequeue();
+            }
+        }
+        else 
+        {
+            Console.WriteLine("Queue contains \"{0}\": False", search);
+        }
+        return aQueue;
+    }
+}
