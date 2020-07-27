84bad63b15dee5146ecd26877f04f5393a3a3026
Anthony <huggins9000211@gmail.com>
2020-07-27 13:27:37 -0400

    test

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int x = 0;
+        List<int> newL = new List<int>();
+        foreach (int item in myList)
+        {
+            if (!newL.Contains(item))
+            {
+                newL.Add(item);
+            }
+        }
+        foreach (int item in newL)
+        {
+            x += item;
+        }
+        return x;
+    }
+}
