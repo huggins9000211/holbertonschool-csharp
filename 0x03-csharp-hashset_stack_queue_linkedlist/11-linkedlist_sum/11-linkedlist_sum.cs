675e90a804705889f535f9be5b5dbb5378b4f99f
Anthony <huggins9000211@gmail.com>
2020-07-27 16:01:51 -0400

    test

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int[] iArray = new int[myLList.Count];
+        myLList.CopyTo(iArray, 0);
+        int i = 0;
+        foreach (int x in iArray)
+        {
+            i += x;
+        }
+        return i;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-main.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    // Main - entry point
+    static void Main(string[] args)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        myLList.AddLast(21);
+        myLList.AddLast(9);
+        myLList.AddLast(-8);
+        myLList.AddLast(54);
+        myLList.AddLast(23);
+        myLList.AddLast(-6);
+        myLList.AddLast(5);
+
+        Console.WriteLine(LList.Sum(myLList));
+    }
+}
