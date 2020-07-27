eef4f66667c077d286411da7bdf1cb1f17f45a5f
Anthony <huggins9000211@gmail.com>
2020-07-27 15:53:17 -0400

    test

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int[] iArray = new int[myLList.Count];
+        myLList.CopyTo(iArray, 0);
+        return iArray[n];
+    }
+}
