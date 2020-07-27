21e0ec9fcd3c7ff1729c45a8c29aaa4e15a8b42d
Anthony <huggins9000211@gmail.com>
2020-07-27 15:35:02 -0400

    test

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int x = myLList.Find(value);
+        return x;
+    }
+}
