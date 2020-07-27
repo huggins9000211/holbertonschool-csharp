2a46f079ead0b055b6d23f6efe2f5e41cfd7be27
Anthony <huggins9000211@gmail.com>
2020-07-27 14:52:33 -0400

    test

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int i = 0;
+        while (true)
+        {
+            try
+            {
+                myLList.RemoveFirst();
+                i ++;
+            }
+            catch (InvalidOperationException)
+            {
+                return i;
+            }
+        }
+    }
+}
