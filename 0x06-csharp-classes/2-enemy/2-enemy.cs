c58b84d3659c3b7d9134f4917e1c0660dea9f573
Anthony <huggins9000211@gmail.com>
2020-07-30 12:05:11 -0400

    test

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -2,13 +2,26 @@
 
 namespace Enemies
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Zombie
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public int health;
+        /// <summary>
+        /// 
+        /// </summary>
         public Zombie()
         {
             this.health = 0;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
         public Zombie(int value)
         {
             if (value >= 0)
