b5c3e22e4f1417a29023589b4de2843fb69cb604
Anthony <huggins9000211@gmail.com>
2020-08-06 14:48:56 -0400

    test

---
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Shape 
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get => width;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+
+    public new int Area()
+    {
+        return Width * Height;
+    }
+
+    public override String ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
+    }
+}
+
+class Square : Rectangle
+{
+    private int size;
+
+    public int Size
+    {
+        get => size;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            else
+                size = value;
+                Height = value;
+                Width = value;
+        }
+    }
+
+    public override String ToString()
+    {
+        return $"[Square] {width} / {height}";
+    }
+}
