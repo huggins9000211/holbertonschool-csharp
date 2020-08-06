41cbfd35fd027206e9b0ac2d63f3ef4434c82a46
Anthony <huggins9000211@gmail.com>
2020-08-06 14:07:29 -0400

    test

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        //Type x = myObj.GetType();
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
+        IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach (PropertyInfo p in pList)
+        {
+            if (!p.Name.Contains("System."))
+            {
+                Console.WriteLine(p.Name);
+            }
+        }
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        foreach (MethodInfo m in mList)
+        {
+            if (!m.Name.Contains("System."))
+            {
+                Console.WriteLine(m.Name);
+            }
+            //Console.Write(m.DeclaringType.Name);
+            // Console.Write(m.DeclaringType.Name);
+            // Console.Write(" ::: ");
+            // Console.WriteLine(m.Name);
+        }
+    }
+}
