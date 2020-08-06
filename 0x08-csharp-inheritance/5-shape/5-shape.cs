d37bcdb2b7f6eb8e922bfa13518651c5bb029ecc
Anthony <huggins9000211@gmail.com>
2020-08-06 14:26:23 -0400

    test

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Shape 
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
