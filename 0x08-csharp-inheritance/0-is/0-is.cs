3f4dc5c95aa409f78fc514b89b13bf2da02c7e8d
Anthony <huggins9000211@gmail.com>
2020-08-06 13:10:41 -0400

    test

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOfTypeInt(object obj)
+    {
+        Type t = obj.GetType();
+        if (t == typeof(int))
+        {
+            return true;
+        }
+        return false;
+    }
+}
