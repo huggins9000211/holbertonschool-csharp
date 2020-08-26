0666b5ddcd4ac3761c1aa952893dd83b53686990
Anthony <huggins9000211@gmail.com>
2020-08-26 13:53:01 -0400

    test

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -6,17 +6,23 @@ class VectorMath
     {
         double[] test = new double[] {3, 9};
         double[] test2 = new double[] {7, -3};
-        Console.WriteLine(Add(test, test2));
+        double[] resut = Add(test, test2);
+        foreach(var item in resut)
+            {
+                Console.WriteLine(item);
+            }
     }
     public static double[] Add(double[] vector1, double[] vector2)
     {
         if (vector1.Length == 2 && vector2.Length == 2)
         {
-            return (new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1]});
+            double[] resut = new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1]};
+            return (resut);
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
-            return (new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]});
+            double[] resut = new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]};
+            return (resut);
         }
         return(new double[] {-1});
     }
