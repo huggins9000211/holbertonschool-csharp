099f2937ab08fe57bec46b6a6c0f0fd0d8677b5b
Anthony <huggins9000211@gmail.com>
2020-08-26 13:11:45 -0400

    test

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,13 +6,13 @@
 /// </summary>
 class VectorMath
 {
-    // static void Main(string[] args)
-    // {
-    //     double[] test = new double[] {3, 9};
-    //     double[] test2 = new double[] {7, -3, -9};
-    //     Console.WriteLine(Magnitude(test));
-    //     Console.WriteLine(Magnitude(test2));
-    // }
+    static void Main(string[] args)
+    {
+        double[] test = new double[] {3, 9};
+        double[] test2 = new double[] {7, -3, -9};
+        Console.WriteLine(Magnitude(test));
+        Console.WriteLine(Magnitude(test2));
+    }
 
     /// <summary>
     /// 
@@ -23,11 +23,11 @@ class VectorMath
     {
         if (vector.Length == 2)
         {
-            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 2);
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 1);
         }
         else if (vector.Length == 3)
         {
-            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 2);
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 1);
         }
         return(-1);
     }
