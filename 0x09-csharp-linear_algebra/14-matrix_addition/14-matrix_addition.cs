b16a7ae024e3e88a536b5f6b67ccff82bec941d5
Anthony <huggins9000211@gmail.com>
2020-08-26 20:58:58 -0400

    test

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.Length == 2 && matrix2.Length == 2)
+        {
+            double[,] result = new double[,] { {matrix1[0,0] + matrix2[0,0], matrix1[0,1] + matrix2[0,1]},
+            {matrix1[1,0] + matrix2[1,0], matrix1[1,1] + matrix2[1,1]}};
+        }
+        else if (matrix1.Length == 3 && matrix2.Length == 3)
+        {
+            double[,] result = new double[,] { {matrix1[0,0] + matrix2[0,0], matrix1[0,1] + matrix2[0,1], matrix1[0,2] + matrix2[0,2]},
+            {matrix1[1,0] + matrix2[1,0], matrix1[1,1] + matrix2[1,1], matrix1[1,2] + matrix2[1,2]}, 
+            {matrix1[2,0] + matrix2[2,0], matrix1[2,1] + matrix2[2,1], matrix1[2,2] + matrix2[2,2]}};
+        }
+        return new double[,] { {0} };
+    }
+}
