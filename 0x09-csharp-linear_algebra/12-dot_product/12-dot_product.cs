4ab55a75a43389048e39e5eaf6bc8520aee19c67
Anthony <huggins9000211@gmail.com>
2020-08-26 20:30:40 -0400

    test

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -5,19 +5,13 @@
 /// </summary>
 class VectorMath
 {
-    // static void Main(string[] args)
-    // {
-    //     double[] test = new double[] {3, 9};
-    //     double[] test2 = new double[] {7, -3, -9};
-    //     Console.WriteLine(Magnitude(test));
-    //     Console.WriteLine(Magnitude(test2));
-    // }
+    static void Main(string[] args)
+    {
+        double[] test = new double[] {3, 9};
+        double[] test2 = new double[] {7, -3};
+        Console.WriteLine(DotProduct(test, test2));
+    }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="vector"></param>
-    /// <returns></returns>
     public static double DotProduct(double[] vector1, double[] vector2)
     {
         if (vector1.Length == 2 && vector2.Length == 2)
