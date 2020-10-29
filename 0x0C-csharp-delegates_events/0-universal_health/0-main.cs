7a3eeb47d8dbb6f92e1706f74a59624082cc9462
Anthony <huggins9000211@gmail.com>
2020-10-28 21:01:50 -0400

    test

--- 0x0C-csharp-delegates_events/0-universal_health/0-main.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-main.cs
@@ -1,13 +1,13 @@
-using System;
+// using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Player player1 = new Player("Electric Mouse", 500f);
-        Player player2 = new Player("Water Turtle");
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Player player1 = new Player("Electric Mouse", 500f);
+//         Player player2 = new Player("Water Turtle");
 
-        player1.PrintHealth();
-        player2.PrintHealth();
-    }
-}
+//         player1.PrintHealth();
+//         player2.PrintHealth();
+//     }
+// }
