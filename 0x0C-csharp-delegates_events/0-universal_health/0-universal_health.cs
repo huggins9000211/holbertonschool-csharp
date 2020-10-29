0bb114b1f53c7cd14263d432a36451a74730c60f
Anthony <huggins9000211@gmail.com>
2020-10-28 20:57:38 -0400

    test

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = this.maxHp;
+        this.name = name;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+    // public string name
+    // {
+    //     get => name;
+    //     set
+    //     {
+    //         name = value;
+    //     }
+    // }
+    // public float maxHp
+    // {
+    //     get => name;
+    //     set
+    //     {
+    //         name = value;
+    //     }
+    // }
+    // public float hp
+    // {
+    //     get => name;
+    //     set
+    //     {
+    //         name = value;
+    //     }
+    // }
+}
