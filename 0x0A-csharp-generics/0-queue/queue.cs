5d8782315f01360eb34544603269f46f62444bf2
Anthony <huggins9000211@gmail.com>
2021-03-22 20:10:04 -0400

    test

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -1,7 +1,14 @@
 ﻿using System;
-
+/// <summary>
+/// test
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class Queue<T>
 {
+    /// <summary>
+    /// test
+    /// </summary>
+    /// <returns></returns>
     public System.Type CheckType()
     {
         return typeof(T);
