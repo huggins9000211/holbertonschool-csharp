24f19ab8f209886b66f105740fe6d14183599a48
Anthony <huggins9000211@gmail.com>
2021-03-25 21:02:47 -0400

    test

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+/// <summary>
+/// test
+/// </summary>
+class Player
+{
+    //Test
+    private string name;
+    //Test
+    private float maxHp;
+    //Test
+    private float hp;
+
+    /// <summary>
+    /// test
+    /// </summary>
+    /// <param name="name">test</param>
+    /// <param name="maxHp">test</param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = this.maxHp;
+        this.name = name;
+    }
+    public delegate void CalculateHealth(float amount);
+
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+            damage = 0;
+        Console.WriteLine($"{this.name} takes {damage} damage!");
+    }
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+            heal = 0;
+        Console.WriteLine($"{this.name} heals {heal} HP!");
+    }
+    /// <summary>
+    /// test
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
